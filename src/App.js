0c8a685a4f7cd6db5c47a40d115f66cdf9b1b715
Chrissy <c.atherholt@gmail.com>
2020-04-22 13:02:07 -0400

    made more progress on my movie app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Nav from "./components/nav.js"
-import SearchBar from "./components/search-bar"
+import SearchBar from "./components/search-bar.js"
+import MovieList from "./components/movie-list.js"
+import Pagination from "./components/pagination.js"
+import MovieInfo from "./components/movie-info.js"
+
 
 class App extends Component {
   
@@ -8,7 +12,11 @@ class App extends Component {
     super()
     this.state = {
       movies: [],
-      searchTerm: ""
+      searchTerm: "",
+      totalResults: 0,
+      currentPage: 1,
+      currentMovie: null
+
     }
     this.apiKey = process.env.REACT_APP_API
   }
@@ -16,11 +24,10 @@ class App extends Component {
   handleSubmit = (e) => {
 
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}`)
-
     .then(data => data.json())
     .then(data => {
       console.log(data);
-      this.setState({ movies: [...data.results]})
+      this.setState({ movies: [...data.results], totalResults: data.total_results})
     })
  
     e.preventDefault()
@@ -30,11 +37,39 @@ class App extends Component {
     this.setState({ searchTerm: e.target.value })
   }
 
+  nextPage = (pageNumber) => {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}&page${pageNumber}`)
+    .then(data => data.json())
+    .then(data => {
+      console.log(data);
+      this.setState({ movies: [...data.results], currentPage: pageNumber })
+    })
+  }
+
+  viewMovieInfo = (id) => {
+    const filterMovie = this.state.movies.filter(movie => movie.id === id);
+
+    const newCurrentMovie = filteredMovie.length > 0 ? filteredMovie[0] : null; 
+
+    this.setState({ currentMovie: filteredMovie })
+
+  }
+
+  closeMovieInfo = () => {
+      this.setState({ currentMovie: null })
+  }
+
   render() {
+    const numberPages = Math.floor(this.state.totalResults / 20);
+
     return (
       <div className="App">
         <Nav />
-        <SearchBar handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
+
+        {this.state.currentMovie === null ? <div><SearchBar handleSubmit={this.handleSubmit} handleChange={this.handleChange}/><MovieList viewMovieInfo={this.viewMovieInfo} movies={this.state.movies} /></div> : <MovieInfo closeMovieInfo={this.closeMovieInfo} />}
+
+        {this.state.totalResults > 20 ? <Pagination pages={numberPages} nextPage={this.nextPage} currentPage={this.state.currentPage} /> : "" }
+
       </div>
     );
   }
---
+++ src/components/movie-info.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MovieInfo = (props) => {
+    return (
+        <div></div>
+    )
+}
+
+export default MovieInfo;
